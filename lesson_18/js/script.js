d18996eb508ae375171a36628c8f126495015bcb
Volodymyr Yermakov <96197315+VovaFlashMan@users.noreply.github.com>
2023-04-10 16:39:37 +0200

    Delete lesson_18 directory

--- lesson_18/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("click", function(e) {
-    const targetElement = e.target;
-
-    if (targetElement.closest('.header__burger-btn')) {
-        document.documentElement.classList.toggle('menu-open');
-        e.preventDefault();
-    }
-
-})
