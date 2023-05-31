dcfaa8ac82081980afc2e9ab39bf783be83080cb
Volodymyr Yermakov <96197315+VovaFlashMan@users.noreply.github.com>
2023-05-31 16:08:22 +0200

    Delete lesson_21 directory

--- lesson_21/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("click", function(e) {
-    const targetElement = e.target;
-    if (targetElement.closest('.menu__button')) {
-        document.documentElement.classList.toggle('menu-open');
-        e.preventDefault();
-    }
-})
